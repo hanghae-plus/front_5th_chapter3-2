3082d23530f27f1d74a34a40f30ec090bd77acdc
ui-choiseoeun <sechoi@sinsiway.com>
2025-05-19 17:10:55 +0900

    fix : 테스트케이스 수정

---
+++ src/__tests__/hooks/useRecurringEvents.spec.ts
@@ -0,0 +1,13 @@
+import { renderHook } from '@testing-library/react';
+import { useRecurringEvents } from '@/hooks/useRecurringEvents';
+import { Event } from '@/types';
+
+describe('useRecurringEvents Hook', () => {
+  it('반복 일정 유무에 따라 isRecurring값이 적용 되야 한다.', () => {});
+
+  it('이벤트가 없는 경우 빈배열을 반환해야한다.', () => {});
+
+  it('반복 이벤트는 generateRecurringEvents로 확장된 일정 데이터를 가진다', () => {});
+
+  it('반복 이벤트가 아닌 경우 instances는 undefined', () => {});
+});
