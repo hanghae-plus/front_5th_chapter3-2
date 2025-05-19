ec05fcb40d779ecc58b8893aa9e0a6b39dbb7e53
hsw824 <ghdtjddn0824@naver.com>
2025-05-20 01:49:07 +0900

    feat: getRepeatingEvents 매일 반복 로직 기초 작성

--- src/utils/getRepeatingEvents.ts
+++ src/utils/getRepeatingEvents.ts
@@ -1,5 +1,21 @@
 import { Event, EventForm } from '../types';
+import { formatDate, getDaysInMonth } from './dateUtils';
 
 export const getRepeatingEvents = (eventData: Event | EventForm) => {
-  return eventData;
+  const eventList: Event[] = [];
+  const eventDate = new Date(eventData.date);
+  const days = getDaysInMonth(eventDate.getFullYear(), eventDate.getMonth() + 1);
+
+  if (eventData.repeat.type === 'daily') {
+    // 해당 월로 넘어가면 그 때 생성 제너레이터?
+    const limitedNum = new Date(eventData.date).getDate();
+    const date = new Date(eventData.date);
+
+    for (let i = limitedNum; i <= days; i++) {
+      date.setDate(i);
+
+      eventList.push({ ...eventData, id: '2', date: formatDate(date) });
+    }
+  }
+  return eventList;
 };
