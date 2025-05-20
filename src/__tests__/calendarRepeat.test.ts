0c17aa4c4f7e62737b5c1c47c1d758c4510f2f60
hsw824 <ghdtjddn0824@naver.com>
2025-05-20 23:52:46 +0900

    test: 필수 요구사항2 failed test 구현

--- src/__tests__/calendarRepeat.test.ts
+++ src/__tests__/calendarRepeat.test.ts
@@ -15,7 +15,9 @@ describe('매일 반복', () => {
       repeat: { type: 'daily', interval: 0 },
       notificationTime: 1,
     };
+
     const result = getRepeatingEvents(event);
+
     expect(result).toContainEqual({
       id: 'fixed-id',
       title: 'test title',
@@ -26,6 +28,36 @@ describe('매일 반복', () => {
       location: 'test home',
       category: '업무',
       repeat: { type: 'daily', interval: 0 },
+      notificationTime: 1,
+    });
+  });
+
+  it('등록한 일정이 2일마다 반복된다.', () => {
+    const event: Event = {
+      id: '2',
+      title: 'test title',
+      date: '2025-05-19',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'daily', interval: 2 },
+      notificationTime: 1,
+    };
+
+    const result = getRepeatingEvents(event);
+
+    expect(result).not.toContainEqual({
+      id: 'fixed-id',
+      title: 'test title',
+      date: '2025-05-20',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'daily', interval: 2 },
       notificationTime: 1,
     });
   });
@@ -62,6 +94,35 @@ describe('매주 반복', () => {
       notificationTime: 1,
     });
   });
+
+  it('등록한 일정이 2주마다 반복된다.', () => {
+    const event: Event = {
+      id: '2',
+      title: 'test title',
+      date: '2025-05-19',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'weekly', interval: 2 },
+      notificationTime: 1,
+    };
+    const result = getRepeatingEvents(event);
+
+    expect(result).not.toContainEqual({
+      id: 'fixed-id',
+      title: 'test title',
+      date: '2025-05-26',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'weekly', interval: 2 },
+      notificationTime: 1,
+    });
+  });
 });
 
 describe('매월 반복', () => {
@@ -84,6 +145,35 @@ describe('매월 반복', () => {
       id: 'fixed-id',
       title: 'test title',
       date: '2025-06-19',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'monthly', interval: 0 },
+      notificationTime: 1,
+    });
+  });
+
+  it('등록한 일정이 2달마다 반복된다.', () => {
+    const event: Event = {
+      id: '2',
+      title: 'test title',
+      date: '2025-05-19',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'monthly', interval: 2 },
+      notificationTime: 1,
+    };
+    const result = getRepeatingEvents(event);
+
+    expect(result).toContainEqual({
+      id: 'fixed-id',
+      title: 'test title',
+      date: '2025-07-19',
       startTime: '10:00',
       endTime: '11:00',
       description: 'test description',
@@ -179,6 +269,35 @@ describe('매년 반복', () => {
       location: 'test home',
       category: '업무',
       repeat: { type: 'yearly', interval: 0 },
+      notificationTime: 1,
+    });
+  });
+
+  it('등록한 일정이 2년 주기로 반복된다.', () => {
+    const event: Event = {
+      id: '2',
+      title: 'test title',
+      date: '2025-05-19',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'yearly', interval: 2 },
+      notificationTime: 1,
+    };
+    const result = getRepeatingEvents(event);
+
+    expect(result).not.toContainEqual({
+      id: 'fixed-id',
+      title: 'test title',
+      date: '2026-05-19',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: 'test description',
+      location: 'test home',
+      category: '업무',
+      repeat: { type: 'yearly', interval: 2 },
       notificationTime: 1,
     });
   });
