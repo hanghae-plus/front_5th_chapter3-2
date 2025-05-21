c0874941ccbde848453cdea24b98156e58abc2a7
mini--inim <forestaroundseoul@gmail.com>
2025-05-22 03:53:58 +0900

    api handler 추가

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -16,72 +16,15 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
       newEvent.id = String(mockEvents.length + 1); // 간단한 ID 생성
       mockEvents.push(newEvent);
       return HttpResponse.json(newEvent, { status: 201 });
-    })
-  );
-};
-
-export const setupMockHandlerUpdating = () => {
-  const mockEvents: Event[] = [
-    {
-      id: '1',
-      title: '기존 회의',
-      date: '2025-10-15',
-      startTime: '09:00',
-      endTime: '10:00',
-      description: '기존 팀 미팅',
-      location: '회의실 B',
-      category: '업무',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 10,
-    },
-    {
-      id: '2',
-      title: '기존 회의2',
-      date: '2025-10-15',
-      startTime: '11:00',
-      endTime: '12:00',
-      description: '기존 팀 미팅 2',
-      location: '회의실 C',
-      category: '업무 회의',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 5,
-    },
-  ];
-
-  server.use(
-    http.get('/api/events', () => {
-      return HttpResponse.json({ events: mockEvents });
     }),
-    http.put('/api/events/:id', async ({ params, request }) => {
-      const { id } = params;
-      const updatedEvent = (await request.json()) as Event;
-      const index = mockEvents.findIndex((event) => event.id === id);
-
-      mockEvents[index] = { ...mockEvents[index], ...updatedEvent };
-      return HttpResponse.json(mockEvents[index]);
-    })
-  );
-};
-
-export const setupMockHandlerDeletion = () => {
-  const mockEvents: Event[] = [
-    {
-      id: '1',
-      title: '삭제할 이벤트',
-      date: '2025-10-15',
-      startTime: '09:00',
-      endTime: '10:00',
-      description: '삭제할 이벤트입니다',
-      location: '어딘가',
-      category: '기타',
-      repeat: { type: 'none', interval: 0 },
-      notificationTime: 10,
-    },
-  ];
-
-  server.use(
-    http.get('/api/events', () => {
-      return HttpResponse.json({ events: mockEvents });
+    http.post('/api/events-list', async ({ request }) => {
+      const { events: newEvents } = (await request.json()) as { events: Event[] };
+      const repeatedEvents = newEvents.map((event, index) => ({
+        ...event,
+        id: String(mockEvents.length + index + 1),
+      }));
+      mockEvents.push(...repeatedEvents);
+      return HttpResponse.json(repeatedEvents, { status: 201 });
     }),
     http.delete('/api/events/:id', ({ params }) => {
       const { id } = params;
@@ -89,6 +32,109 @@ export const setupMockHandlerDeletion = () => {
 
       mockEvents.splice(index, 1);
       return new HttpResponse(null, { status: 204 });
+    }),
+    http.put('/api/events/:id', async ({ params, request }) => {
+      const { id } = params;
+      const updatedEvent = (await request.json()) as Event;
+      const index = mockEvents.findIndex((event) => event.id === id);
+      mockEvents[index] = { ...mockEvents[index], ...updatedEvent };
+      return HttpResponse.json(mockEvents[index]);
+    }),
+    http.put('/api/events-list', async ({ request }) => {
+      const { events } = (await request.json()) as { events: Event[] };
+      events.forEach((event) => {
+        const index = mockEvents.findIndex((e) => e.id === event.id);
+        mockEvents[index] = { ...mockEvents[index], ...event };
+      });
+      return HttpResponse.json(events, { status: 200 });
+    })
+  );
+};
+
+export const setupMockHandlerDeletionAllRepeatEvents = () => {
+  let mockEvents: Event[] = [
+    {
+      id: '1',
+      title: '기존 회의',
+      date: '2025-10-20',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { id: 'repeat-id', type: 'weekly', interval: 1, 'endDate': '2025-10-30'},
+      notificationTime: 10,
+    },
+    {
+      id: '2',
+      title: '기존 회의',
+      date: '2025-10-27',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { id: 'repeat-id', type: 'daily', interval: 1, 'endDate': '2025-10-30'},
+      notificationTime: 10,
+    },
+  ];
+
+  server.use(
+    http.get('/api/events', () => {
+      return HttpResponse.json({ events: mockEvents });
+    }),
+    http.delete('/api/events-list', async ({ request }) => {
+      const { eventIds } = (await request.json()) as { eventIds: string[] };
+
+      const newEvents = mockEvents.filter((event) => !eventIds.includes(event.id));
+      mockEvents = [...newEvents];
+
+      return new HttpResponse(null, { status: 204 });
+    })
+  );
+};
+
+export const setupMockHandlerUpdatingAllRepeatEvents = () => {
+  let mockEvents: Event[] = [
+    {
+      id: '1',
+      title: '기존 회의',
+      date: '2025-10-20',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { id: 'repeat-id', type: 'weekly', interval: 1, 'endDate': '2025-10-30'},
+      notificationTime: 10,
+    },
+    {
+      id: '2',
+      title: '기존 회의',
+      date: '2025-10-27',
+      startTime: '09:00',
+      endTime: '10:00',
+      description: '기존 팀 미팅',
+      location: '회의실 B',
+      category: '업무',
+      repeat: { id: 'repeat-id', type: 'daily', interval: 1, 'endDate': '2025-10-30'},
+      notificationTime: 10,
+    },
+  ];
+
+  server.use(
+    http.get('/api/events', () => {
+      return HttpResponse.json({ events: mockEvents });
+    }),
+    http.put('/api/events-list', async ({ request }) => {
+      const { events } = (await request.json()) as { events: Event[] };
+
+      events.forEach((event) => {
+        const index = mockEvents.findIndex((e) => e.id === event.id);
+        mockEvents[index] = { ...mockEvents[index], ...event };
+      });
+
+      return HttpResponse.json(events, { status: 200 });
     })
   );
 };
