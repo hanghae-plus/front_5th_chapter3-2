9490425dc47509fb50b205349a90a3c7007a36a1
chaaerim <cofla411@ewhain.net>
2025-05-21 17:02:48 +0900

    test: get api 및 event render e2e test code 구현

--- src/__tests__/e2e/app.spec.ts
+++ src/__tests__/e2e/app.spec.ts
@@ -1,6 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('홈페이지 렌더링', async ({ page }) => {
-  // 개발 서버 먼저 켜 두세요: pnpm start
-  await page.goto('/');
+import { Event } from '../../types';
+import { getFilteredEvents } from '../../utils/eventUtils';
+
+test('1. GET API를 통해 저장된 Event List를 가져와서 렌더 한다.', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  const toast = page.getByText('일정 로딩 완료!').first();
+  await expect(toast).toBeVisible();
+
+  const response = await fetch('http://localhost:5173/api/events');
+  const { events } = await response.json();
+
+  const eventList = getFilteredEvents(events, '', new Date(), 'month');
+  const eventListView = page.getByTestId('event-list');
+
+  eventList.forEach(async (event: Event) => {
+    await expect(eventListView.getByText(event.title).first()).toBeVisible();
+  });
 });
