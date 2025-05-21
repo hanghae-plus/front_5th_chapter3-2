2d0639a401c9832c1aed0e869615bb576a7703bc
Paul Kang <kbu715@gmail.com>
2025-05-21 21:57:09 +0900

    feat: createRepeatEvents 유틸

--- src/utils/eventUtils.ts
+++ src/utils/eventUtils.ts
@@ -1,4 +1,5 @@
-import { Event } from '../types';
+import { Event, EventForm } from '../types';
+import { addDays, addWeeks, addMonths, addYears, format } from 'date-fns';
 import { getWeekDates, isDateInRange } from './dateUtils';
 
 function filterEventsByDateRange(events: Event[], start: Date, end: Date): Event[] {
@@ -48,3 +49,55 @@ export function getFilteredEvents(
 
   return searchedEvents;
 }
+
+export const isLeapYear = (year: number) => {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+
+export const createRepeatEvents = (event: Event | EventForm) => {
+  const { repeat, date } = event;
+  const { type, interval, endDate } = repeat;
+
+  let currentDate = new Date(date);
+  const repeatEndDate = endDate ? new Date(endDate) : new Date('2025-12-31');
+  const repeatEvents: Event[] = [];
+
+  while (currentDate <= repeatEndDate) {
+    repeatEvents.push({
+      ...(event as Event),
+      date: format(currentDate, 'yyyy-MM-dd'),
+    });
+
+    switch (type) {
+      case 'daily':
+        currentDate = addDays(currentDate, interval);
+        break;
+      case 'weekly':
+        currentDate = addWeeks(currentDate, interval);
+        break;
+      case 'monthly':
+        currentDate = addMonths(currentDate, interval);
+        break;
+      case 'yearly': {
+        const nextYear = currentDate.getFullYear() + interval;
+        const nextMonth = currentDate.getMonth();
+        const nextDay = currentDate.getDate();
+
+        // 2월 29일인 경우 다음 해가 윤년이 아닐 때 2월 28일로 조정
+        if (nextMonth === 1 && nextDay === 29) {
+          if (!isLeapYear(nextYear)) {
+            currentDate = new Date(nextYear, nextMonth, 28);
+            break;
+          }
+        }
+
+        currentDate = new Date(nextYear, nextMonth, nextDay);
+        break;
+      }
+      default:
+        return repeatEvents;
+    }
+  }
+
+  return repeatEvents;
+};
