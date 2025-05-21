01559dd169fb73cef1a58ad1e531c038ea1baee8
SeongYoonMin <namo4862@naver.com>
2025-05-22 01:37:06 +0900

    FIX

---
+++ src/__tests__/repeat.integration.spec.tsx
@@ -0,0 +1,73 @@
+import { render, screen, act } from '@testing-library/react';
+
+describe('반복 유형 선택', () => {
+  it('일정 생성 시 반복 일정 체크 박스가 존재한다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('일정 수정 시 반복 일정 체크 박스가 존재한다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('반복 일정 체크 시 반복 유형 Select가 존재한다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('반복 유형 선택 시 매일, 매주, 매월, 매년 옵션이 제공된다.', () => {
+    expect(true).toBe(false);
+  });
+});
+
+describe('반복 간격 설정', () => {
+  it('반복 일정 체크 시 반복 간격이 존재한다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('반복 간격 입력 후 저장 시 해당 옵션에 맞는 반복 간격이 입력된다.', () => {
+    expect(true).toBe(false);
+  });
+});
+
+describe('반복 일정 표시', () => {
+  it('반복 일정 추가 시 캘린더의 해당 일자에 R 표시가 추가된다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('등록된 일정이 반복 일정이 아닐 경우 R 표시가 추가되지 않는다.', () => {
+    expect(true).toBe(false);
+  });
+});
+
+describe('반복 일정 삭제', () => {
+  it('반복 일정 삭제 시 반복 일정이 삭제된다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('반복 일정 삭제 시 캘린더에 반복 일정 R 표시가 삭제 된다.', () => {
+    expect(true).toBe(false);
+  });
+});
+
+describe('반복 일정 수정', () => {
+  it('반복 일정 수정 시 반복 일정이 수정된다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('반복 일정 수정 시 캘린더에 반복 일정 R 표시가 수정된다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('반복 일정 수정 시 반복 일정을 해제 할 경우 단일 일정이 된다.', () => {
+    expect(true).toBe(false);
+  });
+
+  it('반복 일정 수정 시 반복 일정을 해제 할 경우 캘린더에 반복 일정 R 표시가 삭제된다.', () => {
+    expect(true).toBe(false);
+  });
+});
+
+describe('반복 일정 단일 삭제', () => {
+  it('반복 일정 단일 삭제 시 해당 일정이 삭제된다.', () => {
+    expect(true).toBe(false);
+  });
+});
