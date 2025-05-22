a53707bd95fc03bda38007a6f5b482a353c71c9f
SoominYim <soo15soo@kakao.com>
2025-05-23 00:55:55 +0900

    chore: Playwright 설정에서 테스트 파일을 순차 실행하도록 workers 수 변경

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './src/e2e',
+  workers: 1, // 모든 테스트 파일을 순차 실행
   use: {
     baseURL: 'http://localhost:5173',
     headless: true,
