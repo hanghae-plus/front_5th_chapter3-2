ce6fd72dbf3c8c0b0bde464b3efd9ea596e29c59
SeongYoonMin <namo4862@naver.com>
2025-05-23 02:51:29 +0900

    feat: repeatUtils 함수 추가

---
+++ src/utils/repeatUtils.ts
@@ -0,0 +1,8 @@
+export const getRepeatDates = (
+  today: Date,
+  repeatOptions: { type: string; interval: number; endDate?: Date; count?: number }
+) => {
+  console.log('Date', today);
+  console.log('repeatOptions', repeatOptions);
+  return [];
+};
