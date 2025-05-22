b8a30532c6cc3657c0e7a669ff0a5014aa61e59e
ssp <qkrtjdtlr02@naver.com>
2025-05-22 16:16:24 +0900

    e2e 초안 설정

--- e2e/app.spec.ts
+++ e2e/app.spec.ts
@@ -1,11 +1,28 @@
 import { test, expect } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-});
+test.describe('App 전체 테스트', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173/reset');
+    await page.goto('http://localhost:5173/');
+  });
+  test('단일 일정 추가 테스트', async ({ page }) => {
+    await page.getByRole('textbox', { name: '제목' }).click();
+    await page.getByRole('textbox', { name: '제목' }).fill('테스트');
+    await page.getByRole('textbox', { name: '날짜' }).fill('2025-05-22');
+    await page.getByRole('textbox', { name: '시작 시간' }).click();
+    await page.getByRole('textbox', { name: '시작 시간' }).fill('10:00');
+    await page.getByRole('textbox', { name: '종료 시간' }).click();
+    await page.getByRole('textbox', { name: '종료 시간' }).fill('11:00');
+    await page.getByRole('textbox', { name: '설명' }).click();
+    await page.getByRole('textbox', { name: '설명' }).fill('테스트 일정');
+    await page.getByRole('textbox', { name: '위치' }).click();
+    await page.getByRole('textbox', { name: '위치' }).fill('테스트 위치');
+    await page.getByLabel('카테고리').selectOption('업무');
 
-test.describe('E2E 기능 테스트', () => {
-  test('타이틀 텍스트 체크', async ({ page }) => {
-    await expect(page).toHaveTitle(/일정관리 앱으로 학습하는 테스트 코드/);
+    await page.getByText('반복 일정').click();
+
+    await page.getByTestId('event-submit-button').click();
+
+    await expect(page.getByTestId('month-view').getByText('테스트')).toBeVisible();
   });
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -71,9 +71,16 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    port: 5173,
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: [
+    {
+      command: 'npm run dev',
+      port: 5173,
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: 'node e2eServer.js',
+      port: 3000,
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
 });
