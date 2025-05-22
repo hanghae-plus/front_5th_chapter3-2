09167648a7df2044a12a50a43b84e1b0bfb97d7a
Paul Kang <kbu715@gmail.com>
2025-05-23 00:08:02 +0900

    chore: e2e 테스트를 위한 playwright 환경 설정, 기본 테스트에서 e2e 테스트 제외, ci 설정

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/__tests__/e2e',
+  fullyParallel: true,
+  timeout: 30_000,
+  retries: 1,
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'pnpm run dev',
+    port: 5173,
+    reuseExistingServer: true,
+  },
+});
